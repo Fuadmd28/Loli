6529783d84a5c2ddaf06d6d53514676080b3e3c9
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2024-07-10 17:59:18 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; PEMILIK BOT\nNICKNAME: PEMILIK BOT\nORG: PEMILIK BOT\nTITLE:\nitem1.TEL;waid=6288980860521:+62 889-8086-0521\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://www.zeltoria.my.id\nitem2.EMAIL;type=INTERNET: elistz21@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;ðŸ‡®ðŸ‡© Indonesia;;;;\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; PEMILIK BOT\nNICKNAME: PEMILIK BOT\nORG: PEMILIK BOT\nTITLE:\nitem1.TEL;waid=6282147781510:+62 821-4778-1510\nitem1.X-ABLabel: Nomor Owner\nitem2.URL:https://www.zeltoria.my.id\nitem2.EMAIL;type=INTERNET: elistz21@gmail.com\nitem2.X-ABLabel:\nitem4.ADR:;;ðŸ‡®ðŸ‡© Indonesia;;;;\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 m.reply(`Hay Kak Itu Ownerku, Jangan Macam~Macam Ya`)
 }
